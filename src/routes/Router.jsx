d6477a073df460648db1c0849c86bd1e9608aca1
marcoselopez <emmanuel.m.lopez@gmail.com>
2022-08-22 13:13:22 -0300

    navbar created

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import Home from "../components/Home/Home"
 import Login from "../components/Login/Login"
+import Navbar from "../components/Navbar/Navbar"
 import Register from "../components/Register/Register"
 
 const Router = () => {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
